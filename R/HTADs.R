1e5922bf9c3a14826b9be7a06ffb445a35455d3a
nanakiksc <polcusco@gmail.com>
2019-04-11 12:50:20 +0200

    Updated docs. Some cleanup.

--- R/HTADs.R
+++ R/HTADs.R
@@ -57,103 +57,104 @@ load_mat <- function(input_data, bad_frac = 0.01, plot_hist = FALSE, check = TRU
 }
 
 sparse_cor <- function(x) {
-  # Create a sparse correlation matrix.
-  covmat <- (crossprod(as.matrix(x)) - nrow(x) * tcrossprod(colMeans(x))) / (nrow(x) - 1)
-  sdvec <- sqrt(diag(covmat))
-  cormat <- covmat / tcrossprod(sdvec)
-  list(cov = covmat, cor = cormat)
+    # Create a sparse correlation matrix.
+    covmat <- (crossprod(as.matrix(x)) - nrow(x) * tcrossprod(colMeans(x))) / (nrow(x) - 1)
+    sdvec <- sqrt(diag(covmat))
+    cormat <- covmat / tcrossprod(sdvec)
+    list(cov = covmat, cor = cormat)
 }
 
 find_params_accurate <- function(pca, number_pca, cores, min_clusters) {
-  doParallel::registerDoParallel(cores = cores)
-  calinhara_score <- foreach::`%dopar%`(foreach::foreach(i = 1:number_pca), {
-    pcs <- as.matrix(pca$x[, 1:i])
-    row.names(pcs) <- 1:nrow(pcs)
+    doParallel::registerDoParallel(cores = cores)
+    calinhara_score <- foreach::`%dopar%`(foreach::foreach(i = 1:number_pca), {
+        pcs <- as.matrix(pca$x[, 1:i])
+        row.names(pcs) <- 1:nrow(pcs)
 
-    clust <- rioja::chclust(dist(pcs))
+        clust <- rioja::chclust(dist(pcs))
 
-    # Broken stick.
-    bs <- rioja::bstick(clust, nrow(pcs) - 1, plot = FALSE)
-    r <- rle(bs$dispersion > bs$bstick)
-    n_cluster <- r$lengths[r$values][1]
+        # Broken stick.
+        bs <- rioja::bstick(clust, nrow(pcs) - 1, plot = FALSE)
+        r <- rle(bs$dispersion > bs$bstick)
+        n_cluster <- r$lengths[r$values][1]
 
-    score <- rep(NA, n_cluster)
-    min_clusters <- min(min_clusters, n_cluster)
-    for (n in min_clusters:n_cluster) {
-      chc <- cutree(clust, k = n)
-      score[n] <- fpc::calinhara(pca$x, chc, cn = n)
-    }
+        score <- rep(NA, n_cluster)
+        min_clusters <- min(min_clusters, n_cluster)
+        for (n in min_clusters:n_cluster) {
+            chc <- cutree(clust, k = n)
+            score[n] <- fpc::calinhara(pca$x, chc, cn = n)
+        }
 
-    score
-  })
+        score
+    })
 
-  scores <- matrix(NA, nrow = length(calinhara_score), ncol = max(sapply(calinhara_score, length)))
-  for (pc in 1:length(calinhara_score)) scores[pc, 1:length(calinhara_score[[pc]])] <- calinhara_score[[pc]]
-  rownames(scores) <- 1:number_pca
-  colnames(scores) <- 1:ncol(scores) + 1
+    scores <- matrix(NA, nrow = length(calinhara_score), ncol = max(sapply(calinhara_score, length)))
+    for (pc in 1:length(calinhara_score)) scores[pc, 1:length(calinhara_score[[pc]])] <- calinhara_score[[pc]]
+    rownames(scores) <- 1:number_pca
+    colnames(scores) <- 1:ncol(scores) + 1
 
-  optimal_PCs <- which.max(rowMeans(scores, na.rm = TRUE))
-  optimal_n_clusters <- which.max(scores[optimal_PCs, ])
-  message(paste('Optimal number of PCs:', optimal_PCs))
-  message(paste('Optimal number of clusters:', optimal_n_clusters))
+    optimal_PCs <- which.max(rowMeans(scores, na.rm = TRUE))
+    optimal_n_clusters <- which.max(scores[optimal_PCs, ])
+    message(paste('Optimal number of PCs:', optimal_PCs))
+    message(paste('Optimal number of clusters:', optimal_n_clusters))
 
-  list(n_PCs = optimal_PCs, n_clusters = optimal_n_clusters, scores = scores)
+    list(n_PCs = optimal_PCs, n_clusters = optimal_n_clusters, scores = scores)
 }
 
 find_params_fast <- function(pca, number_pca, n_samples, min_clusters) {
-  n_pca <- sample(number_pca, n_samples, replace = TRUE)
-  n_clu <- rep(NA, n_samples)
-  calinhara_score <- list()
+    n_pca <- sample(number_pca, n_samples, replace = TRUE)
+    n_clu <- rep(NA, n_samples)
+    calinhara_score <- list()
 
-  for (i in 1:n_samples) {
-    pcs <- as.matrix(pca$x[, 1:n_pca[i]])
-    row.names(pcs) <- 1:nrow(pcs)
+    for (i in 1:n_samples) {
+        pcs <- as.matrix(pca$x[, 1:n_pca[i]])
+        row.names(pcs) <- 1:nrow(pcs)
 
-    clust <- rioja::chclust(dist(pcs))
+        clust <- rioja::chclust(dist(pcs))
 
-    # Broken stick.
-    bs <- rioja::bstick(clust, nrow(pcs) - 1, plot = FALSE)
-    r <- rle(bs$dispersion > bs$bstick)
-    n_cluster <- r$lengths[r$values][1]
+        # Broken stick.
+        bs <- rioja::bstick(clust, nrow(pcs) - 1, plot = FALSE)
+        r <- rle(bs$dispersion > bs$bstick)
+        n_cluster <- r$lengths[r$values][1]
 
-    n_clu[i] <- sample(min_clusters:n_cluster, 1)
-    chc <- cutree(clust, k = n_clu[i])
-    calinhara_score[[i]] <- fpc::calinhara(pca$x, chc, cn = n_clu[i])
-  }
+        n_clu[i] <- sample(min_clusters:n_cluster, 1)
+        chc <- cutree(clust, k = n_clu[i])
+        calinhara_score[[i]] <- fpc::calinhara(pca$x, chc, cn = n_clu[i])
+    }
 
-  scores <- matrix(NA, nrow = max(n_pca), ncol = max(n_clu))
-  for (i in 1:n_samples) scores[n_pca[i], n_clu[i]] <- calinhara_score[[i]]
-  rownames(scores) <- 1:max(n_pca)
-  colnames(scores) <- 1:max(n_clu)
+    scores <- matrix(NA, nrow = max(n_pca), ncol = max(n_clu))
+    for (i in 1:n_samples) scores[n_pca[i], n_clu[i]] <- calinhara_score[[i]]
+    rownames(scores) <- 1:max(n_pca)
+    colnames(scores) <- 1:max(n_clu)
 
-  optimal_PCs <- which.max(rowMeans(scores, na.rm = TRUE))
-  optimal_n_clusters <- which.max(scores[optimal_PCs, ])
-  message(paste('Optimal number of PCs:', optimal_PCs))
-  message(paste('Optimal number of clusters:', optimal_n_clusters))
+    optimal_PCs <- which.max(rowMeans(scores, na.rm = TRUE))
+    optimal_n_clusters <- which.max(scores[optimal_PCs, ])
+    message(paste('Optimal number of PCs:', optimal_PCs))
+    message(paste('Optimal number of clusters:', optimal_n_clusters))
 
-  list(n_PCs = optimal_PCs, n_clusters = optimal_n_clusters, scores = scores)
+    list(n_PCs = optimal_PCs, n_clusters = optimal_n_clusters, scores = scores)
 }
 
 #' Save Bed file with the TAD's borders coordinates.
 #'
 #' @param htads `htad` object returned by `call_HTADs`.
-#' @param chrom `character` with the name of the chromosome("chrX").
-#' @param file path to save the file in bed format.
+#' @param chrom `character`. The name of the chromosome.
+#' @param file either a character string naming a file or a `connection` open for writing. `""` indicates output to the console.
 #' @examples
 #' load('data/chromosome18_10Mb.Rdata')
 #' htads <- call_HTADs(chromosome18_10Mb)
-#' save_bed_coordinates(htads,chrom = "chr18","/scratch/User/chr18_borders.bed")
+#' save_bed_coordinates(htads, chrom = 'chr18', file = 'chr18_borders.bed')
 #' @export
 
-save_bed_coordinates <- function(htads, chrom, output){
-    start_coord <- htads$clusters[[as.character(htads$optimal_n_clusters)]]$coord$start
-    end_coord <- htads$clusters[[as.character(htads$optimal_n_clusters)]]$coord$end
-    bed_format <- cbind(chrom,start_coord,end_coord)
-    write.table(bed_format,output,
-                col.names = F,
-                row.names = F,
-                quote = F, sep = "\t")}
-
+save_bed_coordinates <- function(htads, chrom, file = '') {
+    start <- htads$clusters[[as.character(htads$optimal_n_clusters)]]$coord$start
+    end <- htads$clusters[[as.character(htads$optimal_n_clusters)]]$coord$end
+    write.table(cbind(chrom, start, end),
+                file = file,
+                quote = FALSE,
+                sep = '\t',
+                row.names = FALSE,
+                col.names = FALSE)
+}
 
 #' Plot a heatmap of the Caliski-Harabasz index of every tested `n_pcs`/`n_clusters` combination
 #'
@@ -166,14 +167,14 @@ save_bed_coordinates <- function(htads, chrom, output){
 #' @export
 
 plot_scores <- function(htads, file = NULL) {
-  # Plot nPCs vs nClusters CHi.
-  s <- htads$scores
-  p <- plotly::layout(plotly::plot_ly(z = s, type = "heatmap"),
-                      title = 'Caliski-Harabasz index',
-                      xaxis = list(title = 'Number of clusters'),
-                      yaxis = list(title = 'Number of PCs'))
-  if (!is.null(file)) htmlwidgets::saveWidget(p, file)
-  p
+    # Plot nPCs vs nClusters CHi.
+    s <- htads$scores
+    p <- plotly::layout(plotly::plot_ly(z = s, type = "heatmap"),
+                        title = 'Caliski-Harabasz index',
+                        xaxis = list(title = 'Number of clusters'),
+                        yaxis = list(title = 'Number of PCs'))
+    if (!is.null(file)) htmlwidgets::saveWidget(p, file)
+    p
 }
 
 #' Plot TAD borders on a HiC matrix
@@ -227,27 +228,27 @@ plot_dendro <- function(htads) {
 #' @export
 
 plot_var <- function(input_data, max_pcs = NULL, mark = 200, percent = 0.8) {
-  mat <- load_mat(input_data, percent)[[1]]
+    mat <- load_mat(input_data, percent)[[1]]
 
-  # Sparse matrix and correlation.
-  correlation_matrix <- sparse_cor(mat)$cor
-  correlation_matrix[is.na(correlation_matrix)] <- 0 # Clean NA/NaN values.
+    # Sparse matrix and correlation.
+    correlation_matrix <- sparse_cor(mat)$cor
+    correlation_matrix[is.na(correlation_matrix)] <- 0 # Clean NA/NaN values.
 
-  # PCA (compute first `number_pca` components).
-  if (is.null(max_pcs)) max_pcs <- nrow(mat)
-  number_pca <- min(max_pcs, nrow(mat))
-  pca <- prcomp(correlation_matrix, rank. = number_pca)
+    # PCA (compute first `number_pca` components).
+    if (is.null(max_pcs)) max_pcs <- nrow(mat)
+    number_pca <- min(max_pcs, nrow(mat))
+    pca <- prcomp(correlation_matrix, rank. = number_pca)
 
-  perc <- cumsum(pca$sdev^2) / sum(pca$sdev^2) * 100
+    perc <- cumsum(pca$sdev^2) / sum(pca$sdev^2) * 100
 
-  ggpubr::ggline(data.frame('variable' = 1:length(perc),
-                            'value' = perc),
-         x = 'variable',
-         y = 'value',
-         plot_type = 'l',
-         legend = 'none') +
+    ggpubr::ggline(data.frame('variable' = 1:length(perc),
+                              'value' = perc),
+                   x = 'variable',
+                   y = 'value',
+                   plot_type = 'l',
+                   legend = 'none') +
     ggplot2::geom_segment(ggplot2::aes(x = mark, y = 0, xend = mark, yend = 100),
-                         color = 'black', linetype = 'dotted', size = 0.1)
+                          color = 'black', linetype = 'dotted', size = 0.1)
 }
 
 #' Call hierarchical TADs
@@ -268,67 +269,67 @@ plot_var <- function(input_data, max_pcs = NULL, mark = 200, percent = 0.8) {
 #' @export
 
 call_HTADs <- function(input_data, cores = 1, max_pcs = 200, method = c('accurate', 'fast'), n_samples = 60, min_clusters = 1, bad_frac = 0.01, check = TRUE) {
-  # Load and clean data.
-  mat <- load_mat(input_data, bad_frac = bad_frac, check = check)
-  bad_columns <- attr(mat, 'bad_columns')
+    # Load and clean data.
+    mat <- load_mat(input_data, bad_frac = bad_frac, check = check)
+    bad_columns <- attr(mat, 'bad_columns')
 
-  # Sparse matrix and correlation.
-  correlation_matrix <- sparse_cor(mat)$cor
-  correlation_matrix[is.na(correlation_matrix)] <- 0 # Clean NA/NaN values.
+    # Sparse matrix and correlation.
+    correlation_matrix <- sparse_cor(mat)$cor
+    correlation_matrix[is.na(correlation_matrix)] <- 0 # Clean NA/NaN values.
 
-  # PCA (compute first `number_pca` components).
-  number_pca <- min(max_pcs, nrow(mat))
-  pca <- prcomp(correlation_matrix, rank. = number_pca)
+    # PCA (compute first `number_pca` components).
+    number_pca <- min(max_pcs, nrow(mat))
+    pca <- prcomp(correlation_matrix, rank. = number_pca)
 
-  # Find optimal clustering parameters based on Calinhara score.
-  method <- match.arg(method)
-  optimal_params <- switch(method,
-                           accurate = find_params_accurate(pca, number_pca, cores, min_clusters),
-                           fast = find_params_fast(pca, number_pca, n_samples, min_clusters))
+    # Find optimal clustering parameters based on Calinhara score.
+    method <- match.arg(method)
+    optimal_params <- switch(method,
+                             accurate = find_params_accurate(pca, number_pca, cores, min_clusters),
+                             fast = find_params_fast(pca, number_pca, n_samples, min_clusters))
 
-  # Cluster the PCs subset with the best mean-CHI criterion.
-  pcs <- as.matrix(pca$x[, 1:optimal_params$n_PCs])
-  clust <- rioja::chclust(dist(pcs))
+    # Cluster the PCs subset with the best mean-CHI criterion.
+    pcs <- as.matrix(pca$x[, 1:optimal_params$n_PCs])
+    clust <- rioja::chclust(dist(pcs))
 
-  # Create hierarchical cluster object.
-  htads <- structure(list('n_pcs' = optimal_params$n_PCs[[1]],
-                          'optimal_n_clusters' = optimal_params$n_clusters[[1]],
-                          'dendro' = clust,
-                          'clusters' = list(),
-                          'scores' = optimal_params$scores),
-                     class = 'htads')
+    # Create hierarchical cluster object.
+    htads <- structure(list('n_pcs' = optimal_params$n_PCs[[1]],
+                            'optimal_n_clusters' = optimal_params$n_clusters[[1]],
+                            'dendro' = clust,
+                            'clusters' = list(),
+                            'scores' = optimal_params$scores),
+                       class = 'htads')
 
-  for (k in which(!is.na(optimal_params$scores[optimal_params$n_PCs, ]))) {
-      good_clusters <- cutree(clust, k = k)
+    for (k in which(!is.na(optimal_params$scores[optimal_params$n_PCs, ]))) {
+        good_clusters <- cutree(clust, k = k)
 
-      if (!is.null(bad_columns)) {
-          # Add bad columns to the clusters.
-          bad_clusters <- rep(0, length(bad_columns))
-          names(bad_clusters) <- bad_columns
+        if (!is.null(bad_columns)) {
+            # Add bad columns to the clusters.
+            bad_clusters <- rep(0, length(bad_columns))
+            names(bad_clusters) <- bad_columns
 
-          # Merge bad columns with the original data.
-          clusters <- c(good_clusters, bad_clusters)
-          clusters <- clusters[order(as.numeric(names(clusters)))]
+            # Merge bad columns with the original data.
+            clusters <- c(good_clusters, bad_clusters)
+            clusters <- clusters[order(as.numeric(names(clusters)))]
 
-          rle_clusters <- fix_values(rle(clusters))
-          fixed_clusters <- inverse.rle(rle_clusters)
+            rle_clusters <- fix_values(rle(clusters))
+            fixed_clusters <- inverse.rle(rle_clusters)
 
-          eb <- cumsum(rle(fixed_clusters)$length)
-          coord <- data.frame('start' = c(1, eb[-length(eb)] + 1, use.names = FALSE),
-                              'end' = eb)
-          coord <- coord[rle(fixed_clusters)$values != 0, ]
-      } else {
-          eb <- cumsum(table(good_clusters))
-          coord <- data.frame('start' = c(1, eb[-length(eb)] + 1, use.names = FALSE),
-                              'end' = eb)
-      }
+            eb <- cumsum(rle(fixed_clusters)$length)
+            coord <- data.frame('start' = c(1, eb[-length(eb)] + 1, use.names = FALSE),
+                                'end' = eb)
+            coord <- coord[rle(fixed_clusters)$values != 0, ]
+        } else {
+            eb <- cumsum(table(good_clusters))
+            coord <- data.frame('start' = c(1, eb[-length(eb)] + 1, use.names = FALSE),
+                                'end' = eb)
+        }
 
-      row.names(coord) <- NULL
-      htads$clusters[[as.character(k)]] <- list('CH-index' = optimal_params$scores[optimal_params$n_PCs, k],
-                                                'coord' = coord)
-  }
+        row.names(coord) <- NULL
+        htads$clusters[[as.character(k)]] <- list('CH-index' = optimal_params$scores[optimal_params$n_PCs, k],
+                                                  'coord' = coord)
+    }
 
-  htads
+    htads
 }
 
 fix_values <- function(r) {
