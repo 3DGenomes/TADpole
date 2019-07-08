ced44c6898304294252d8a984c4b11b31a07dce6
nanakiksc <polcusco@gmail.com>
2019-07-08 17:06:06 +0200

    Using extdata.

--- data.R
+++ data.R
@@ -3,39 +3,24 @@
 #' Contains HiC data of a 74 Mb portion of the chromosome 18
 #' in the human cell line GM12878, at a resolution of 40 kb.
 #'
-#' @format A data frame with 3411409 rows and 3 columns:
-#' \describe{
-#'   \item{First column}{index of the first bin in the interaction pair}
-#'   \item{Second column}{index of the second bin in the interaction pair}
-#'   \item{Third column}{normalized interaction score}
-#' }
-#' @source RAO \url{}
-'chromosome18_74Mb.tsv.gz'
+#' @format A 2006x2006 tab-delimited numeric matrix.
+#' @source Rao et al. \url{https://www.ebi.ac.uk/ena/data/view/SRR1658602}
+'chromosome18_74Mb.tsv'
 
 #' Chromosome 18 (10 Mb).
 #'
 #' Contains HiC data of a 10 Mb portion of the chromosome 18
 #' in the human cell line GM12878, at a resolution of 40 kb.
 #'
-#' @format A data frame with 63001 rows and 3 columns:
-#' \describe{
-#'   \item{First column}{index of the first bin in the interaction pair}
-#'   \item{Second column}{index of the second bin in the interaction pair}
-#'   \item{Third column}{normalized interaction score}
-#' }
-#' @source RAO \url{}
-'chromosome18_10Mb.tsv.gz'
+#' @format A 251x251 tab-delimited numeric matrix.
+#' @source Rao et al. \url{https://www.ebi.ac.uk/ena/data/view/SRR1658602}
+'chromosome18_10Mb.tsv'
 
 #' Chromosome 18 (6 Mb).
 #'
 #' Contains HiC data of a 6 Mb portion of the chromosome 18
 #' in the human cell line GM12878, at a resolution of 40 kb.
 #'
-#' @format A data frame with 22801 rows and 3 columns:
-#' \describe{
-#'   \item{First column}{index of the first bin in the interaction pair}
-#'   \item{Second column}{index of the second bin in the interaction pair}
-#'   \item{Third column}{normalized interaction score}
-#' }
-#' @source RAO \url{}
-'chromosome18_6Mb.tsv.gz'
+#' @format A 151x151 tab-delimited numeric matrix.
+#' @source Rao et al. \url{https://www.ebi.ac.uk/ena/data/view/SRR1658602}
+'chromosome18_6Mb.tsv'
